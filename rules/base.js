ba76ef9c472cb3741ad253aa7a105002128b95e9
Vadim Tarasenko <v.tarasenko@seedium.io>
2020-11-01 21:34:31 +0200

    feat(): version bump

--- rules/base.js
+++ rules/base.js
@@ -42,7 +42,7 @@ module.exports = {
         'no-unsafe-negation': 'error',
         'no-global-assign': 'error',
         'object-curly-newline': ['error', { 'consistent': true }],
-        'lines-between-class-members': ['error', 'always'],
+        'lines-between-class-members': ['error', 'always', { "exceptAfterSingleLine": true }],
         'array-callback-return': 'error',
         'default-param-last': 'error',
         'no-import-assign': 'error',
